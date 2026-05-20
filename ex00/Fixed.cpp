5645a66f01a2f08055ad31f17c3e76b1a833713e
Flog <kaoru.kido@fuji.waseda.jp>
2026-05-20 21:57:17 +0900

    ex00 done

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: kkido <kkido@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/17 14:36:27 by kkido             #+#    #+#             */
-/*   Updated: 2026/05/20 17:45:05 by kkido            ###   ########.fr       */
+/*   Updated: 2026/05/20 21:54:26 by kkido            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,27 @@
 #include "iostream"
 
 Fixed::Fixed() : _fixed_point_num(0){
-	std::cout << "Default constructor calle" << std::endl;
+	std::cout << "Default constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed &src){
-	std::cout << "Copy constructor called." << std::endl;
+	std::cout << "Copy constructor called" << std::endl;
 	this->_fixed_point_num = src.getRawBits();
 }
 
 Fixed& Fixed::operator = (const Fixed &src){
 	std::cout << "Copy assignment operator called" << std::endl;
+	if(this != &src)
+		this->_fixed_point_num = src.getRawBits();
 	return *this;
 }
 
 Fixed::~Fixed(){
-	std::cout << "Desutructor called" << std::endl;
+	std::cout << "Destructor called" << std::endl;
 }
 
 int Fixed::getRawBits(void) const{
+	std::cout << "getRawBits member function called" << std::endl;
 	return _fixed_point_num;
 }
 
